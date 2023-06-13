bdd1f5a9e369af558b7545e6107316ae2d11f88e
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-13 13:47:13 +0300

    + Test_Show_Keys.test_show_keys_case1

---
+++ crispy/tests/test_show_keys.py
@@ -0,0 +1,16 @@
+#
+#       Ferit Yiğit BALABAN,    <fybalaban@fybx.dev>
+#       crispy                  2023
+#
+#       test_show_keys.py
+from unittest import TestCase
+from crispy.crispy import Crispy
+
+
+class Test_Show_Keys(TestCase):
+    def test_show_keys_case1(self):
+        c = Crispy()
+        c.add_variable("name", str)
+        expected = "-n, --name: name\n"
+        actual = c.show_keys()
+        self.assertEqual(expected, actual)
