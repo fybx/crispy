39767dde34f4f19aba8f24f44d3f6c090fef2b2e
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-06-15 05:38:05 +0300

    ^ fix imports

--- crispy/tests/test_add_variable.py
+++ crispy/tests/test_add_variable.py
@@ -6,7 +6,7 @@
 from unittest import TestCase
 
 from crispy.crispy import Crispy
-from crispy.duplicate_name_exception import DuplicateNameException
+from duplicate_name_exception import DuplicateNameException
 
 
 class Test_Add_Variable(TestCase):
